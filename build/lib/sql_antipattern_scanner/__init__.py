4644a73fabc627f9de17fcdbcbeb9359ea3e3f7c
Nicholas Piesco <ngpiesco@gmail.com>
2024-10-09 17:54:26 -0400

    Remove build directory from Git tracking

--- build/lib/sql_antipattern_scanner/__init__.py
+++
@@ -1,5 +0,0 @@
-# sql_antipattern_scanner/sql_antipattern_scanner/__init__.py
-from .sql_antipattern_scanner import *
-from .antipatterns import *
-from .cli import *
-from .report_generator import *
